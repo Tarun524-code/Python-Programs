f173f0d665eaf2c8ac069d73198d62be3244c172
Tarun <tarunsriramnambada@gmail.com>
2026-06-22 12:23:41 +0530

    Reverse the Linked List

--- UserDefined_DataStructures/reverseLinkedList.py
+++ UserDefined_DataStructures/reverseLinkedList.py
@@ -5,18 +5,19 @@ values = list(map(int, input().split()))
 
 for v in values:
     l.insert_end(v)
-
 l.traverse()
 
-c = l.head
-count = 0
-while c is not None:
-    count += 1
-    c = c.next
+prev = None
+curr = l.head
+while curr != None:
+    n = curr.next
+    curr.next = prev
+    prev = curr
+    curr = n 
+l.head = prev
 
-c = l.head
-
-for _ in range(count // 2):
-    c = c.next
-
-print("Middle node value:", c.data if c else None)
+print('Reversed list:')
+if l.head is None:
+    print('None')
+else:
+    l.traverse
