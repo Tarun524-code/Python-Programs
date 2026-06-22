c91059f1c78b270ffba5a63006ac91a2839a9572
Tarun <tarunsriramnambada@gmail.com>
2026-06-22 12:24:27 +0530

    Two pass

--- UserDefined_DataStructures/TwoPass.py
+++ UserDefined_DataStructures/TwoPass.py
@@ -5,15 +5,18 @@ values = list(map(int, input().split()))
 
 for v in values:
     l.insert_end(v)
+
 l.traverse()
 
 c = l.head
-count  = 0
+count = 0
 while c is not None:
-    count = count + 1
+    count += 1
     c = c.next
+
 c = l.head
 
-for _ in range(0,count//2):
+for _ in range(count // 2):
     c = c.next
-    
+
+print("Middle node value:", c.data if c else None)
