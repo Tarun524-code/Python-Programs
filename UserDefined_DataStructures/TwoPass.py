f613ab1979cb1939ee21517b0483e391bf37580d
Tarun <tarunsriramnambada@gmail.com>
2026-06-22 12:21:34 +0530

    Two pass

--- UserDefined_DataStructures/TwoPass.py
+++ UserDefined_DataStructures/TwoPass.py
@@ -13,7 +13,7 @@ while c is not None:
     count = count + 1
     c = c.next
 c = l.head
+
 for _ in range(0,count//2):
     c = c.next
     
-
--- UserDefined_DataStructures/reverseLinkedList.py
+++ UserDefined_DataStructures/reverseLinkedList.py
@@ -5,19 +5,18 @@ values = list(map(int, input().split()))
 
 for v in values:
     l.insert_end(v)
+
 l.traverse()
 
-prev = None
-curr = l.head
-while curr != None:
-    n = curr.next
-    curr.next = prev
-    prev = curr
-    curr = n 
-l.head = prev
+c = l.head
+count = 0
+while c is not None:
+    count += 1
+    c = c.next
 
-print('Reversed list:')
-if l.head is None:
-    print('None')
-else:
-    l.traverse
+c = l.head
+
+for _ in range(count // 2):
+    c = c.next
+
+print("Middle node value:", c.data if c else None)
